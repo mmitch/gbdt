d706f3705262a906edc09a9e67e55b33a842b319
Christian Garbs <mitch@cgarbs.de>
2018-11-04 21:21:53 +0100

    update copyright

--- README.md
+++ README.md
@@ -160,5 +160,18 @@ The project is hosted at https://github.com/mmitch/gbdt
 copyright
 ---------
 
-Copyright (C) 2015  Christian Garbs <mitch@cgarbs.de>  
+Copyright (C) 2015,2016,2018  Christian Garbs <mitch@cgarbs.de>  
 Licensed under GNU GPL v3 or later.
+
+gbdt is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+gbdt is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with gbdt.  If not, see <http://www.gnu.org/licenses/>.
--- test.sh
+++ test.sh
@@ -1,4 +1,26 @@
 #!/bin/bash
+#
+# test.sh - integration tests for gbdt
+#
+# Copyright (C) 2016,2018  Christian Garbs <mitch@cgarbs.de>
+# Licensed under GNU GPL v3 or later.
+#
+# This file is part of gbdt, see https://github.com/mmitch/gbdt
+#
+# gbdt is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# gbdt is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with gbdt.  If not, see <http://www.gnu.org/licenses/>.
+#
+
 set -e
 
 DIR=$(mktemp -d --tmpdir gbdt-unittest-XXXXXXXX)
